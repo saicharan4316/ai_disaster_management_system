fe4f95efb88529941bccd7792d41494e4bc7248c
sai charan <utukurisaicharan123@gmail.com>
2026-06-06 13:15:59 +0530

    Update API base URL to production Render endpoint

--- client/src/components/services/api.js
+++ client/src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ai-disaster-management-system-c4xm.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
